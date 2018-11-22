40bd07aa6f41bb4c30e289e7e943ab4cb4cf960c
Michelle <michelle.wu177@gmail.com>
2018-11-22 00:23:56 -0800

    deploying to heroku

--- scripts.js
+++ scripts.js
@@ -45,3 +45,23 @@ var makeClosure = function(name) {
 
 // loadJobs(positions)
 // console.log(positions[1])
+
+let aboutLink = document.querySelector("#about");
+// aboutLink.addEventListener("click",function () {
+//    document.querySelector("#about-section").classList = "show" 
+// },false)
+
+
+
+
+let box = document.querySelector(".box");
+
+aboutLink.addEventListener("click",function () {
+
+      ht = `<p><span>Im a </span> Frontend Developer with an entrepreneur and leadership mindset.  Experienced in UX, frontend technologies, Object-Oriented programming, and DOM manipulation. Skilled in taking products from inception through delivery.  Capable of refining every aspect of a project with an objective eye for detail and efficiency mindset.</p></div>`;
+
+      
+  box.innerHTML += ht;
+  console.log(div)
+      // console.log(aboutinfo)
+  },false)
