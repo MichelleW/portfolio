3c60ad96b7eed8fec491953f0db5a3950e20e470
Michelle <michelle.wu177@gmail.com>
2019-01-12 11:44:46 -0800

    -amadding projects and about pages

--- scripts.js
+++ scripts.js
@@ -1,50 +1,24 @@
-let skills = {
-
-}
 
 let positions = ["frontend developer",
 "UI developer",
 "Web developer","bob","worker"]
 
 
-let positionsDom = document.querySelector('.positions');
+let positionsDom = document.querySelector('.typewriter');
 
 let displayPositions = (name)=> positions.innerHTML = name;
 // console.log(displayPositions())
 
-var logName = function(name) {
-  positionsDom.innerHTML = name;
-    console.log(name);
+var logName = (p)=> {
+    var pos = positionsDom.textContent;
+    console.log(positionsDom.textContent);
+
+    return p.forEach((jobs) => {
+        pos = jobs;
+    })
 };
-var makeClosure = function(name) {
-    return function() { 
-        logName(name);
-    }
-};
-// for (var i=0; i < positions.length; i++) {
-//     var name = positions[i];
-//      console.log(i)
-//     setTimeout(makeClosure(name), 4000);
-// }
 
-
-// let makeClosure = function(){
-//   return function () {
-//     displayPositions(name)
-//   }
-// }
-
-  // function loadJobs() { 
-  //   for(let i=0; i<3; i++){
-  //     console.log(positions[i])
-  //     // positionsDom.innerHTML = positions[i]
-  //   }
-  // }
-//  setTimeout(loadJobs, 4000);
-//  setTimeout(() => loadJobs, 1000);
-
-// loadJobs(positions)
-// console.log(positions[1])
+logName(positions)
 
 let aboutLink = document.querySelector("#about");
 // aboutLink.addEventListener("click",function () {
@@ -54,14 +28,18 @@ let aboutLink = document.querySelector("#about");
 
 
 
-let box = document.querySelector(".box");
+let details = document.querySelector(".details");
 
 aboutLink.addEventListener("click",function () {
 
-      ht = `<p><span>Im a </span> Frontend Developer with an entrepreneur and leadership mindset.  Experienced in UX, frontend technologies, Object-Oriented programming, and DOM manipulation. Skilled in taking products from inception through delivery.  Capable of refining every aspect of a project with an objective eye for detail and efficiency mindset.</p></div>`;
+      ht = `<p><span>I'm a </span> Frontend Developer with an entrepreneur and leadership mindset.  Experienced in UX, frontend technologies, Object-Oriented programming, and DOM manipulation. Skilled in taking products from inception through delivery.  Capable of refining every aspect of a project with an objective eye for detail and efficiency mindset.</p></div>`;
 
       
-  box.innerHTML += ht;
+  details.innerHTML = ht;
   console.log(div)
       // console.log(aboutinfo)
   },false)
+
+  document.querySelector("#projects").addEventListener('click',() => {
+      
+  })
